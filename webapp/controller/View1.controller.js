59c4776f942854ee381e636df925b38904bc740d
tomasseckar <tomas.seckar@eviden.com>
2024-05-16 12:12:33 +0000

    show/hide filter/tables based on radio button

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,20 +9,39 @@ sap.ui.define([
 
         return Controller.extend("smarttable.controller.View1", {
             onInit: function () {
+                this.getView().byId("smartFilterBar1").setVisible(true);
+                this.getView().byId("smartFilterBar2").setVisible(false);
+                this.getView().byId("smartTable1").setVisible(true);
+                this.getView().byId("smartTable2").setVisible(false);
 
             },
             onSelect: function () {
                 var selected = this.getView().byId("radioButtonGroup1").getSelectedButton().getText();
                 
                 if (selected==="Fyzická osoba") {
-                    this.getView().byId("smartFilterBar1").setVisible(false);
+                    this.getView().byId("smartFilterBar1").setVisible(true);
+                    this.getView().byId("smartFilterBar2").setVisible(false);
+                    this.getView().byId("smartTable1").setVisible(true);
+                    this.getView().byId("smartTable2").setVisible(false);
                 }
                 else if (selected==="Právnická osoba") {
-                    this.getView().byId("smartFilterBar1").setVisible(true);
+                    this.getView().byId("smartFilterBar1").setVisible(false);
+                    this.getView().byId("smartFilterBar2").setVisible(true);
+                    this.getView().byId("smartTable1").setVisible(false);
+                    this.getView().byId("smartTable2").setVisible(true);
                 }
             },
-            onRowPress: function () {
+            onRowPress1: function () {
                 var table1 = this.getView().byId('table1');
+                var idnsbj1 = table1.getSelectedItem().getBindingContext().getProperty('Idnsbj');
+                var vrzsbj1 = table1.getSelectedItem().getBindingContext().getProperty('Vrzsbj');
+                this.getOwnerComponent().getRouter().navTo("RouteView2",{
+                    Idnsbj : idnsbj1,
+                    Vrzsbj : vrzsbj1
+                });
+            },
+            onRowPress2: function () {
+                var table1 = this.getView().byId('table2');
                 var idnsbj1 = table1.getSelectedItem().getBindingContext().getProperty('Idnsbj');
                 var vrzsbj1 = table1.getSelectedItem().getBindingContext().getProperty('Vrzsbj');
                 this.getOwnerComponent().getRouter().navTo("RouteView2",{
