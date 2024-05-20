d06c77509cf87efc88f72a9c001b7da04fb76f6b
tomasseckar <tomas.seckar@eviden.com>
2024-05-20 10:00:11 +0000

    setSupressSelection added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,11 +9,16 @@ sap.ui.define([
 
         return Controller.extend("smarttable.controller.View1", {
             onInit: function () {
-                this.getView().byId("smartFilterBar1").setVisible(true);
+                this.getView().byId("smartFilterBar1").setVisible(false);
                 this.getView().byId("smartFilterBar2").setVisible(false);
                 this.getView().byId("smartTable1").setVisible(false);
                 this.getView().byId("smartTable2").setVisible(false);
-                /* this.getView().byId("radioButtonGroup1").setSelectedButton("radioButton1"); */
+                this.getView().byId("smartFilterBar1").setSuppressSelection(true);
+                this.getView().byId("smartFilterBar2").setSuppressSelection(true);
+            },
+            onBeforeRendering: function () {
+            },
+            onAfterRendering: function () {
             },
             onSelect: function () {
                 var selected = this.getView().byId("radioButtonGroup1").getSelectedButton().getText();
@@ -23,17 +28,47 @@ sap.ui.define([
                     this.getView().byId("smartFilterBar2").setVisible(false);
                     this.getView().byId("smartTable1").setVisible(false);
                     this.getView().byId("smartTable2").setVisible(false);
+                    this.getView().byId("smartFilterBar1").setSuppressSelection(false);
                 }
                 else if (selected==="Právnická osoba") {
                     this.getView().byId("smartFilterBar1").setVisible(false);
                     this.getView().byId("smartFilterBar2").setVisible(true);
                     this.getView().byId("smartTable1").setVisible(false);
                     this.getView().byId("smartTable2").setVisible(false);
+                    this.getView().byId("smartFilterBar2").setSuppressSelection(false);
                 }
             },
             onGo1: function () {
                 this.getView().byId("smartTable1").setVisible(true);
                 this.getView().byId("smartTable2").setVisible(false);
+
+/*                 var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getControlByKey("Jmnosb");
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
+                debugger;
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getControlConfiguration();
+                debugger;
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getParameters();
+                debugger;
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getFiltersWithValues();
+                debugger;
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getNavigationProperties();
+                debugger;
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getParameters();
+                debugger;
+                this.getView().byId("smartFilterBar1").setSuppressSelection(true);
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getSuppressSelection();
+                debugger;
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
+                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata(); */
+
+
+                
+
+
+                
             },
             onGo2: function () {
                 this.getView().byId("smartTable1").setVisible(false);
@@ -59,3 +94,4 @@ sap.ui.define([
             }
         });
     });
+
