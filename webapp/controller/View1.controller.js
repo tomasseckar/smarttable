5f01009ece237b475733609393da68358066e8b5
tomasseckar <tomas.seckar@eviden.com>
2024-05-20 10:07:46 +0000

    button instead of radiobutton

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -20,55 +20,23 @@ sap.ui.define([
             },
             onAfterRendering: function () {
             },
-            onSelect: function () {
-                var selected = this.getView().byId("radioButtonGroup1").getSelectedButton().getText();
-                
-                if (selected==="Fyzická osoba") {
+            onSelect1: function () {
                     this.getView().byId("smartFilterBar1").setVisible(true);
                     this.getView().byId("smartFilterBar2").setVisible(false);
                     this.getView().byId("smartTable1").setVisible(false);
                     this.getView().byId("smartTable2").setVisible(false);
                     this.getView().byId("smartFilterBar1").setSuppressSelection(false);
-                }
-                else if (selected==="Právnická osoba") {
+            },
+            onSelect2: function () {
                     this.getView().byId("smartFilterBar1").setVisible(false);
                     this.getView().byId("smartFilterBar2").setVisible(true);
                     this.getView().byId("smartTable1").setVisible(false);
                     this.getView().byId("smartTable2").setVisible(false);
                     this.getView().byId("smartFilterBar2").setSuppressSelection(false);
-                }
             },
             onGo1: function () {
                 this.getView().byId("smartTable1").setVisible(true);
-                this.getView().byId("smartTable2").setVisible(false);
-
-/*                 var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getControlByKey("Jmnosb");
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
-                debugger;
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getControlConfiguration();
-                debugger;
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getParameters();
-                debugger;
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getFiltersWithValues();
-                debugger;
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getNavigationProperties();
-                debugger;
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getParameters();
-                debugger;
-                this.getView().byId("smartFilterBar1").setSuppressSelection(true);
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getSuppressSelection();
-                debugger;
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata();
-                var jmnosbFilterValue = this.getView().byId("smartFilterBar1").getMetadata(); */
-
-
-                
-
-
-                
+                this.getView().byId("smartTable2").setVisible(false);           
             },
             onGo2: function () {
                 this.getView().byId("smartTable1").setVisible(false);
